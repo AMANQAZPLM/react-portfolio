5e3974040951890b266f150f2e4711f089dc4bd3
AMANQAZPLM <amanqazplm@gmail.com>
2023-05-04 22:45:24 +0530

    Third commit

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -20,7 +20,7 @@ const Landing = ({ setSelectedPage }) => {
             <img
               alt="profile"
               className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
-              src="assets/landing-2.png"
+              src="assets/landing-2-1.png"
             />
           </div>
         ) : (
